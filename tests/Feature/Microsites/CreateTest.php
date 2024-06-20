e2f46c51d385a691802eb4e216519a38b39687eb
Angelo Caruso <angelocaruso.07@gmail.com>
2024-06-19 22:37:20 -0500

    refactor: fix naming convections

---
+++ tests/Feature/Microsites/CreateTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Microsites;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_logged_user_can_microsites_creation_from(): void
+    {
+
+    }
+}
--- tests/Feature/Microsites/IndexTest.php
+++ tests/Feature/Microsites/IndexTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature\Microsites;
 use App\Models\User;
 use Tests\TestCase;
 
-class Index extends TestCase
+class IndexTest extends TestCase
 {
     public function test_logged_user_can_see_microsites(): void
     {
