2482384b6ebd70da0539dbf33c58d1467e349a73
Angelo Caruso <angelocaruso.07@gmail.com>
2024-06-26 23:20:18 -0500

    feat: added policies for roles crud

---
+++ app/Enums/Roles/RolePermissions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums\Roles;
+
+use Filament\Support\Contracts\HasLabel;
+
+enum RolePermissions: string implements HasLabel
+{
+    case ViewAny = 'roles.view_any';
+    case View = 'roles.view';
+    case Create = 'roles.create';
+    case Update = 'roles.update';
+    case Delete = 'roles.delete';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::ViewAny => 'View Any',
+            self::View => 'View',
+            self::Create => 'Create',
+            self::Update => 'Update',
+            self::Delete => 'Delete',
+        };
+    }
+}
