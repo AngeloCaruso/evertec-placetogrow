42b1e648c26910cb49786daefbad01139f0cf746
Angelo Caruso <angelocaruso.07@gmail.com>
2024-06-26 23:20:52 -0500

    feat: added policies for microsites crud

--- app/Http/Controllers/MicrositeController.php
+++ app/Http/Controllers/MicrositeController.php
@@ -5,31 +5,32 @@ namespace App\Http\Controllers;
 use App\Actions\Microsites\DestroyMicrositeAction;
 use App\Actions\Microsites\GetAllMicrositesAction;
 use App\Models\Microsite;
+use Illuminate\Support\Facades\Gate;
 
 class MicrositeController extends Controller
 {
     public function index()
     {
+        Gate::authorize('viewAny', Microsite::class);
         $sites = GetAllMicrositesAction::exec([], new Microsite());
-        return view('livewire.microsites.index', [
-            'sites' => $sites,
-        ]);
+        return view('livewire.microsites.index', compact('sites'));
     }
 
     public function create()
     {
+        Gate::authorize('create', Microsite::class);
         return view('livewire.microsites.create');
     }
 
     public function edit(Microsite $microsite)
     {
-        return view('livewire.microsites.edit', [
-            'site' => $microsite,
-        ]);
+        Gate::authorize('update', $microsite);
+        return view('livewire.microsites.edit', ['site' => $microsite]);
     }
 
     public function destroy(Microsite $microsite)
     {
+        Gate::authorize('delete', $microsite);
         DestroyMicrositeAction::exec([], $microsite);
         return redirect()->route('microsites.index');
     }
