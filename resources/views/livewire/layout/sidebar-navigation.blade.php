9284934231d149258666bf1ce412dfaecd6304b5
Angelo Caruso <angelocaruso.07@gmail.com>
2024-06-30 10:07:42 -0500

    fix: transitio sidebar responsive

--- resources/views/livewire/layout/sidebar-navigation.blade.php
+++ resources/views/livewire/layout/sidebar-navigation.blade.php
@@ -7,13 +7,26 @@ new class extends Component
 };
 ?>
 
-<nav class="flex flex-1 flex-col">
-    <div id="mobile-menu" class="relative z-50 lg:hidden hidden" role="dialog" aria-modal="true">
-        <div id="backdrop" class="fixed inset-0 bg-gray-900/80 transition-opacity ease-linear duration-300 opacity-0" aria-hidden="true"></div>
-        <div id="off-canvas" class="fixed inset-0 flex -translate-x-full transition ease-in-out duration-300 transform">
-            <div class="relative mr-16 flex w-full max-w-xs flex-1">
-                <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button id="close-button" type="button" class="-m-2.5 p-2.5">
+<nav x-data="{ open: false }" class="flex flex-1 flex-col">
+    <div x-show="open" :class="{'block': open, 'hidden': !open}" class="relative z-50 lg:hidden hidden" role="dialog" aria-modal="true"
+    x-transition:enter="transition-opacity ease-linear duration-100"
+        x-transition:enter-start="opacity-0"
+        x-transition:enter-end="opacity-100"
+        x-transition:leave="transition-opacity ease-linear duration-100"
+        x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0">
+        <div class="fixed inset-0 bg-gray-900/80" aria-hidden="true"
+        x-transition:enter="transition-opacity ease-linear duration-300"
+        x-transition:enter-start="opacity-0"
+        x-transition:enter-end="opacity-100"
+        x-transition:leave="transition-opacity ease-linear duration-300"
+        x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0"
+        ></div>
+        <div class="fixed inset-0 flex">
+            <div class="relative mr-16 flex w-full max-w-xs flex-1" x-transition:enter="transition ease-in-out duration-300 transform" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full">
+                <div class="absolute left-full top-0 flex w-16 justify-center pt-5" x-transition:enter="ease-in-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+                    <button type="button" class="-m-2.5 p-2.5" @click="open = false">
                         <span class="sr-only">Close sidebar</span>
                         <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -27,6 +40,7 @@ new class extends Component
                     <livewire:layout.sidebar-navigation-content />
                 </div>
             </div>
+
         </div>
     </div>
 
@@ -41,11 +55,11 @@ new class extends Component
         </div>
     </div>
 
-    <div class="sticky top-0 z-40 lg:hidden">
-        <button type="button" id="open-button" class="-m-2.5 ml-1.5 p-2.5 text-gray-700">
-            <span class="sr-only">Open sidebar</span>
-            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 6h16.5m-16.5 6h16.5" />
+    <div class="-me-2 flex items-center sm:hidden">
+        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
         </button>
     </div>
