e4e0514a77589c0bfa8a8547ecbe9d80ca68f914
Angelo Caruso <angelocaruso.07@gmail.com>
2024-09-03 14:23:11 -0500

    fix: added acl rules to payments

---
+++ app/Actions/Payments/GetAllPaymentsWithAclAction.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Payments;
+
+use App\Enums\System\AccessRules;
+use App\Models\Microsite;
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+class GetAllPaymentsWithAclAction
+{
+    public static function exec(User $user, Model $model): Builder
+    {
+        $acl = $user->acl()->where('controllable_type', Microsite::class)->get();
+
+        if ($acl->isNotEmpty()) {
+            return $model->query()->whereIn('microsite_id', self::getIds($acl));
+        }
+
+        return Payment::query()->where('email', $user->email);
+    }
+
+    private static function getIds(EloquentCollection $acl): array
+    {
+        $groupedAcl = $acl->groupBy('rule');
+        $allowedIds = self::applyAllow($groupedAcl);
+        $deniedIds = self::applyDeny($groupedAcl);
+
+        return $allowedIds->diff($deniedIds)->toArray();
+    }
+
+    private static function applyAllow(EloquentCollection $acl): Collection
+    {
+        if (!$acl->has(AccessRules::Allow->value)) {
+            return collect([]);
+        }
+        return $acl->get(AccessRules::Allow->value)->pluck('controllable_id');
+    }
+
+    private static function applyDeny(EloquentCollection $acl): Collection
+    {
+        if (!$acl->has(AccessRules::Deny->value)) {
+            return collect([]);
+        }
+        return $acl->get(AccessRules::Deny->value)->pluck('controllable_id');
+    }
+}
