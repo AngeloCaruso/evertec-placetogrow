3f2ff3d5052518ca74b6063e5eccac4b1941ffcf
Angelo Caruso <angelocaruso.07@gmail.com>
2024-10-10 18:59:31 -0500

    chore: added spartan-vue package

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import aspectRatio from '@tailwindcss/aspect-ratio';
 import preset from './vendor/filament/support/tailwind.config.preset'
+import spartanVue from '@placetopay/spartan-vue/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -14,6 +15,7 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        "node_modules/@placetopay/spartan-vue/dist/*.js",
     ],
 
     theme: {
@@ -24,5 +26,5 @@ export default {
         },
     },
 
-    plugins: [forms, aspectRatio],
+    plugins: [forms, aspectRatio, spartanVue],
 };
