ca953117aa634f79948fbc9689f1625590b54764
Angelo Caruso <angelocaruso.07@gmail.com>
2024-10-19 18:51:03 -0500

    fix: lint issues

--- app/Livewire/Dashboard/ActiveVsInactiveSubsChart.php
+++ app/Livewire/Dashboard/ActiveVsInactiveSubsChart.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Livewire\Dashboard;
 
 use Filament\Widgets\ChartWidget;
-use Illuminate\Contracts\Support\Htmlable;
 
 class ActiveVsInactiveSubsChart extends ChartWidget
 {
